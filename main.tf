634831acd6c56f4ad52d919c2bc17227c0c33c2d
Rakesh <rakeshsbali@gmail.com>
2021-11-09 10:36:02 +0530

    added maintf

---
+++ main.tf
@@ -0,0 +1,32 @@
+provider "google" {
+        project = "fourth-silo-325108"
+        region = "us-central1"
+}
+
+resource "google_compute_instance" "myvm" {
+        name = "myfirstvm"
+        machine_type = "f1-micro"
+        zone = "us-central1-a"
+        network_interface {
+                network = "default"
+        }
+        boot_disk {
+                initialize_params {
+                        image = "debian-cloud/debian-10"
+                }
+        }
+}
+
+resource "google_compute_instance" "myvm1" {
+        name = "mysecondvm"
+        machine_type = "f1-micro"
+        zone = "us-central1-a"
+        network_interface {
+                network = "default"
+        }
+        boot_disk {
+                initialize_params {
+                        image = "debian-cloud/debian-10"
+                }
+        }
+}
