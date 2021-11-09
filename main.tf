9200fb443876c31614f8a6fc96bdc71e5d8c8020
Rakesh <rakeshsbali@gmail.com>
2021-11-09 10:56:20 +0530

    3vm

--- main.tf
+++ main.tf
@@ -30,3 +30,18 @@ resource "google_compute_instance" "myvm1" {
                 }
         }
 }
+
+
+resource "google_compute_instance" "myvm2" {
+        name = "my3rdvm"
+        machine_type = "f1-micro"
+        zone = "us-central1-a"
+        network_interface {
+                network = "default"
+        }
+        boot_disk {
+                initialize_params {
+                        image = "debian-cloud/debian-10"
+                }
+        }
+}
